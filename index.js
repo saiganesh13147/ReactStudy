d8b712e69b09fcb9d6c7da04c037137d13ec7c56
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2020-12-29 18:27:10 -0500

    React1

---
+++ index.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Slot Machines!</h1>
+          <Machine
+            s1="🍐"
+            s2="🍐"
+            s3="🍐"
+          />
+          <Machine
+            s1="🍓"
+            s2="🍒"
+            s3="🍊"
+          />
+           <Machine
+            s1="🍌"
+            s2="🍅"
+            s3="🍈"
+          />
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
