8f38f93d38fa671716c09ab5f7209454519b8b61
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2020-12-29 20:05:18 -0500

    Added react files

--- Machine.js
+++ Machine.js
@@ -4,8 +4,9 @@ class Machine extends React.Component {
       const winner = (s1 === s2) && (s2 === s3);
   
       return (
-        <div>
-          <p>{s1} {s2} {s3}</p>
+        <div className="Machine">
+          <p style={{fontSize:'30px', backgroundColor: 'purple'}}>
+              {s1} {s2} {s3}</p>
           <p>{winner ? 'Winner!' : 'Loser!'}</p>
         </div>
       )
